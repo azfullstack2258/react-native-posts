a01ee30424ecad201e0e761fe76e2e6e1bdd581b
Michael Gauthier <mike@silverorange.com>
2020-04-16 18:59:00 -0300

    Add implentation of repos route

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,9 +1,25 @@
-import express from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
+import fetch from 'node-fetch';
 
-export const repos = express.Router();
+const DATA_SOURCE = 'https://api.github.com/users/silverorange/repos';
 
-repos.get('/', (req, res) => {
-  res.header('Content-Type', 'application/json');
-  res.status(200);
-  res.send('[]');
+export const repos = Router();
+
+interface Repo {
+  id: string;
+  fork: boolean;
+}
+
+repos.get('/', async (_: Request, res: Response, next: NextFunction) => {
+  try {
+    const response = await fetch(DATA_SOURCE);
+    const allRepos: Repo[] = await response.json();
+    const onlyForks = allRepos.filter((repo) => !repo.fork);
+
+    res.header('Content-Type', 'application/json');
+    res.status(200);
+    res.send(JSON.stringify(onlyForks));
+  } catch (e) {
+    next(e);
+  }
 });
