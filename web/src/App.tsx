30b783fd557c5b7dee8c82154abc3e207b198e32
Michael Gauthier <mike@silverorange.com>
2020-04-16 17:41:22 -0300

    Use named export

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
+
 import './App.css';
 
-const App: React.FC = () => {
+export function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +22,4 @@ const App: React.FC = () => {
       </header>
     </div>
   );
-};
-
-export default App;
+}
