f3d774e676673380310511ca295528f1ab5d0689
Michael Gauthier <mike@silverorange.com>
2020-04-17 19:01:58 -0300

    Shuffle order of posts in service so sorting is required.

--- api/src/routes/posts.ts
+++ api/src/routes/posts.ts
@@ -1,10 +1,15 @@
 import { Router, Request, Response } from 'express';
 import { posts as postsData } from '../data/posts';
+import shuffle from 'knuth-shuffle-seeded';
 
 export const posts = Router();
 
 posts.get('/', async (_: Request, res: Response) => {
+  shuffle(postsData);
+
   res.header('Content-Type', 'application/json');
+  res.header('Cache-Control', 'no-store');
+
   res.status(200);
   res.send(JSON.stringify(postsData));
 });
